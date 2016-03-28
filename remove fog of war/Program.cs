e8d60ac6edb3f7a6e868f2b1c02853fdf89df5e9
MagicanLyn <magicianlyn@users.noreply.github.com>
2016-03-28 22:09:53 +0900

    release 'remove fog of war' addon

---
+++ remove fog of war/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using EloBuddy;
+
+namespace remove_fog_of_war
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Hacks.DisableTextures = true;
+
+            ManagedTexture.OnLoad += args =>
+            {
+                if (args.Texture.Equals("FogOfWarOverlay.dds", StringComparison.OrdinalIgnoreCase))
+                {
+                    args.Process = false;
+                }
+            };
+        }
+    }
+}
---
+++ remove fog of war/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("remove fog of war")]
+[assembly: AssemblyDescription("remove fog of war")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EloBuddy")]
+[assembly: AssemblyProduct("remove fog of war")]
+[assembly: AssemblyCopyright("Copyright © MagicianLyn.")]
+[assembly: AssemblyTrademark("MagicianLyn")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46bf4c1a-74de-4458-9671-f6f599a84049")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
